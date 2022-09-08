4559bbddd09f2804538b76959c91a1e5210895a8
Francis James Tolentino <francistolentino1107@gmail.com>
2022-09-08 18:27:20 +0800

    Footer Layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import type React from 'react';
+
+const Footer: React.FC<any> = () => {
+  return (
+    <footer className='h-72 border border-l-0 border-r-0 mt-10 border-b-0 bg-gray-100 border-gray-300'>
+    </footer>
+  );
+}
+
+export default Footer;
