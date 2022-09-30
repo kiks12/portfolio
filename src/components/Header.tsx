7826f9300be25dd090c1f2a5a8aeb6f13e4f26b9
Francis James Tolentino <francistolentino1107@gmail.com>
2022-09-30 22:02:48 +0800

    Amazing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 import type React from 'react';
 import Container from './Container';
 import { seePortfolio } from '../components/LandingCTA';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface HeaderProps {
 }
@@ -9,48 +10,144 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = () => {
 
   return (
-    <header className="h-24 flex items-center z-20 ">
-		<nav className="w-100 flex-1">
-			<Container>
-				<div className="flex justify-between">
-					<div className="flex items-center">
-						<h2>
-							<span className="font-black text-gradient">
-								Icon
-							</span>
-						</h2>
-						<ul className="flex mx-10 md:flex sm:hidden items-center">
-							<li className="mx-5 cursor-pointer hover:text-indigo-700" onClick={seePortfolio}>Portfolio</li>
-							<li className="mx-5 cursor-pointer hover:text-indigo-700">About</li>
-						</ul>
-					</div>
-					<ul className="flex">
-						<a href="https://github.com/kiks12">
-							<div className="mx-1 transition-all rounded-full shadow p-2 w-11 h-11 bg-white flex justify-center items-center cursor-pointer hover:shadow-lg">
-								<i className='w-100'>
-									<span><img src="/icons8-github-30.svg"/></span>
-								</i>
+		<AnimatePresence>
+			<header className="h-24 flex items-center z-20 ">
+				<nav className="w-100 flex-1">
+					<Container>
+						<div className="flex justify-between">
+							<div className="flex items-center">
+								<motion.h2
+									animate={{
+										x: 0,
+										opacity: 1,
+									}}
+									initial={{
+										x: -200,
+										opacity: 0,
+									}}
+									transition={{
+										bounce: true,
+										duration: 0.4,
+										delay: 1.4,
+									}}
+								>
+									<span className="font-black text-gradient">
+										Icon
+									</span>
+								</motion.h2>
+								<ul className="flex mx-10 md:flex sm:hidden items-center">
+									<motion.li 
+										animate={{
+											x: 0,
+											opacity: 1,
+										}}
+										initial={{
+											x: -200,
+											opacity: 0,
+										}}
+										transition={{
+											bounce: true,
+											duration: 0.4,
+											delay: 1.3,
+										}}
+										className="mx-5 cursor-pointer hover:text-indigo-700" 
+										onClick={seePortfolio}
+									>
+										Portfolio
+									</motion.li>
+									<motion.li
+										className="mx-5 cursor-pointer hover:text-indigo-700"
+										animate={{
+											x: 0,
+											opacity: 1,
+										}}
+										initial={{
+											x: -200,
+											opacity: 0,
+										}}
+										transition={{
+											bounce: true,
+											duration: 0.4,
+											delay: 1.2,
+										}}
+									>
+										About
+									</motion.li>
+								</ul>
 							</div>
-						</a>
-						<a href="https://www.linkedin.com/in/francis-tolentino-08a428247">
-							<div className="mx-1 transition-all rounded-full shadow p-2 w-11 h-11 bg-white flex justify-center items-center cursor-pointer hover:shadow-lg">
-								<i className='w-100'>
-									<span><img src="/icons8-linkedin-2-30.svg"/></span>
-								</i>
-							</div>
-						</a>
-						<a href="https://twitter.com/francisjames12_">
-							<div className="mx-1 transition-all rounded-full shadow p-2 w-11 h-11 bg-white flex justify-center items-center cursor-pointer hover:shadow-lg">
-								<i className='w-100'>
-									<span><img src="/icons8-twitter-30.svg" /></span>
-								</i>
-							</div>
-						</a>
-					</ul>
-				</div>
-			</Container>
-		</nav>
-	</header> 
+							<ul className="flex">
+								<motion.a 
+									animate={{
+										x: 0,
+										opacity: 1,
+									}}
+									initial={{
+										x: 200,
+										opacity: 0,
+									}}
+									transition={{
+										bounce: true,
+										duration: 0.4,
+										delay: 1.2,
+									}}	
+									href="https://github.com/kiks12"
+								>
+									<div className="mx-1 transition-all rounded-full shadow p-2 w-11 h-11 bg-white flex justify-center items-center cursor-pointer hover:shadow-lg">
+										<i className='w-100'>
+											<span><img src="/icons8-github-30.svg"/></span>
+										</i>
+									</div>
+								</motion.a>
+								<motion.a
+									href="https://www.linkedin.com/in/francis-tolentino-08a428247"
+									animate={{
+										x: 0,
+										opacity: 1,
+									}}
+									initial={{
+										x: 200,
+										opacity: 0,
+									}}
+									transition={{
+										bounce: true,
+										duration: 0.4,
+										delay: 1.3,
+									}}	
+								>
+									<div className="mx-1 transition-all rounded-full shadow p-2 w-11 h-11 bg-white flex justify-center items-center cursor-pointer hover:shadow-lg">
+										<i className='w-100'>
+											<span><img src="/icons8-linkedin-2-30.svg"/></span>
+										</i>
+									</div>
+								</motion.a>
+								<motion.a
+									href="https://twitter.com/francisjames12_"
+									animate={{
+										x: 0,
+										opacity: 1,
+									}}
+									initial={{
+										x: 200,
+										opacity: 0,
+									}}
+									transition={{
+										bounce: true,
+										duration: 0.4,
+										delay: 1.4,
+									}}	
+								>
+									<div className="mx-1 transition-all rounded-full shadow p-2 w-11 h-11 bg-white flex justify-center items-center cursor-pointer hover:shadow-lg">
+										<i className='w-100'>
+											<span><img src="/icons8-twitter-30.svg" /></span>
+										</i>
+									</div>
+								</motion.a>
+							</ul>
+						</div>
+					</Container>
+				</nav>
+			</header> 
+		</AnimatePresence>
   );
 }
 
--- src/components/LandingCTA.tsx
+++ src/components/LandingCTA.tsx
@@ -1,4 +1,5 @@
 import type React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export const seePortfolio = () => {
 		window.scrollTo({
@@ -10,30 +11,121 @@ export const seePortfolio = () => {
 const LandingCTA : React.FC<any> = () => {	
 
   return  (
-    <section className="w-100 flex items-center justify-between" style={{ 'minHeight': '83vh' }}>
-			<div className="flex-1 flex flex-col justify-center items-center">
-				<h1 className="md:text-8xl sm:text-6xl font-semibold text-center tracking-wide leading-none">
-					Hi I'm Francis 
-					<br />
-					James Tolentino
-				</h1>
-				<h2 className="text-base my-9 font-normal tracking-wider text-center">
-					A 🧑🏻‍💻 Fullstack and 📲 Flutter Developer creating web and mobile <br /> apps for small to medium sized businesses
-				</h2>
-				<div className="flex  md:flex-row sm:flex-col-reverse w-100 items-center justify-center mt-8">
-					<button 
-						onClick={seePortfolio}
-						className="px-8 rounded-lg transition-all text-indigo-700 py-2 text-sm sm:mt-4 md:mt-0"
-					>
-						See Portfolio
-					</button>
-          <a href="mailto: francistolentino1107@gmail.com">
-            <button className="bg-indigo-700 border border-indigo-700 hover:bg-indigo-900 transition-all text-white px-8 
-            text-sm rounded-md py-2 ml-3">Let's work together</button>
-          </a>
-				</div>
-			</div>
-		</section>
+		<div className="overflow-hidden">
+			<AnimatePresence>
+				<section className="w-100 flex items-center justify-between" style={{ 'minHeight': '83vh' }}>
+					<div className="flex-1 flex flex-col justify-center items-center">
+						<motion.h1 
+							className="md:text-8xl sm:text-6xl font-semibold text-center tracking-wide leading-none"
+							animate={{
+								y:0, 
+								scale: 1,
+								opacity: 1,
+							}}
+							initial={{
+								y:100,
+								scale: 0.1,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 0.3,
+								delay: 0.1,
+								bounce: true,
+							}}
+						>
+							Hi I'm Francis 
+							<br />
+							James Tolentino
+						</motion.h1>
+						<motion.h2
+							className="text-base my-9 font-normal tracking-wider text-center"
+							animate={{
+								y:0,
+								scale: 1,
+								opacity: 1,
+							}}
+							initial={{
+								y:-100,
+								scale: 0.1,
+								opacity: 0,
+							}}
+							transition={{
+								duration: 0.3,
+								delay: 0.2,
+								bounce: true,
+							}}
+						>
+							A 🧑🏻‍💻 Fullstack and 📲 Flutter Developer creating web and mobile <br /> apps for small to medium sized businesses
+						</motion.h2>
+						<div className="flex  md:flex-row sm:flex-col-reverse w-100 items-center justify-center mt-8">
+							<motion.button 
+								onClick={seePortfolio}
+								className="px-8 rounded-lg transition-all text-indigo-700 py-2 text-sm sm:mt-4 md:mt-0"
+								animate={{
+									x:0,
+									opacity:1,
+								}}
+								initial={{
+									x: 200,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 0.4,
+									delay: 0.5,
+								}}
+							>
+								See Portfolio
+							</motion.button>
+							<motion.a 
+								href="mailto: francistolentino1107@gmail.com"
+								animate={{
+									x:0,
+									opacity:1,
+								}}
+								initial={{
+									x: -200,
+									opacity: 0,
+								}}
+								transition={{
+									duration: 0.4,
+									delay: 0.7,
+								}}
+							>
+								<button className="bg-indigo-700 border border-indigo-700 hover:bg-indigo-900 transition-all text-white px-8 
+								text-sm rounded-md py-2 ml-3">Let's work together</button>
+							</motion.a>
+						</div>
+					</div>
+				</section>
+				<motion.div
+					className='bg-indigo-200 absolute top-0'
+					style={{
+						width:'500px',
+						height: '500px',
+						zIndex:'-1',
+						transform: 'skew(-20deg)'
+					}}
+					></motion.div>
+				<motion.div
+					className='bg-gradient-to-b from-indigo-100 absolute top-40 left-10'
+					style={{
+						width:'400px',
+						height: '400px',
+						zIndex:'-1',
+						transform: 'skew(-20deg)'
+					}}
+					></motion.div>
+				<motion.div
+					className='bg-gradient-to-b from-indigo-400 t-rounded-3xl absolute top-24 right-20'
+					style={{
+						width:'600px',
+						height: '600px',
+						zIndex:'-1',
+						transform: 'skew(-20deg)'
+					}}
+					></motion.div>
+			</AnimatePresence>
+		</div>
   )
 }
 
